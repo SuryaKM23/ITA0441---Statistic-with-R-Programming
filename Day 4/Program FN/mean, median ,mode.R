5b0bf6982d7eb1ad6d2f742414efc49d2d3c687b
Surya K M <115336797+SuryaKM23@users.noreply.github.com>
2023-01-30 18:31:45 +0530

    Delete Day 4 directory

--- Day 4/Program FN/mean, median ,mode.R
+++
@@ -1,8 +0,0 @@
-data <- c(90, 50, 70, 80, 70, 60, 20, 30, 80, 90, 20)
-mean(data)
-median(data)
-mode <- function(x) {
-  uniqx <- unique(x)
-  uniqx[which.max(tabulate(match(x, uniqx)))]
-}
-mode(data)
